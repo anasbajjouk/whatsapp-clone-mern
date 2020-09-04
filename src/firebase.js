8ed56d83488687a222ee14c043de66a5913bd81e
anasbajjouk <a.bajjouk@cleo.one>
2020-09-04 20:29:27 +0200

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCejUuFbjAZBU1z6grhqTb0_rlDmX4I4hM",
+  authDomain: "whatsapp-clone-mern.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-mern.firebaseio.com",
+  projectId: "whatsapp-clone-mern",
+  storageBucket: "whatsapp-clone-mern.appspot.com",
+  messagingSenderId: "389273531895",
+  appId: "1:389273531895:web:fab5ba087884a2eb64aaf2",
+  measurementId: "G-XYZWM3TQ9W",
+};
