bce76c50d85fccba2beb91f03b6052ecb47d90df
Anas Bajjouk <bajjouk.a@office.lan>
2020-09-07 15:58:07 +0200

    finishing up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Chat from "./components/chat/Chat";
 import SideBar from "./components/sideBar/SideBar";
+import Pusher from "pusher-js";
+import axios from "./axios";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    axios.get("/messages/sync").then((response) => setMessages(response.data));
+  }, []);
+
+  useEffect(() => {
+    const pusher = new Pusher("9667449f8d9edfb9e7ef", {
+      cluster: "eu",
+    });
+
+    const channel = pusher.subscribe("messages");
+    channel.bind("inserted", (data) => {
+      setMessages([...messages, data]);
+    });
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, [messages]);
+
   return (
     <div className="app">
       <div className="app__body">
         <SideBar />
-        <Chat />
+        <Chat messages={messages} />
       </div>
     </div>
   );
--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
 import MoreVertOutlinedIcon from "@material-ui/icons/MoreVertOutlined";
 import AttachFileOutlinedIcon from "@material-ui/icons/AttachFileOutlined";
@@ -6,9 +6,25 @@ import MicOutlinedIcon from "@material-ui/icons/MicOutlined";
 import EmojiEmotionsOutlinedIcon from "@material-ui/icons/EmojiEmotionsOutlined";
 import "./Chat.css";
 import { Avatar, IconButton } from "@material-ui/core";
+import axios from "../../axios";
 
-const Chat = () => {
-  const sendMessage = () => {};
+const Chat = ({ messages }) => {
+  const [input, setInput] = useState("");
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    //some Hardcoded values
+    await axios.post("/messages/new", {
+      message: input,
+      name: "Demo name",
+      timestamp: new Date().toUTCString(),
+      received: false,
+    });
+
+    setInput("");
+  };
+
   return (
     <div className="chat">
       <div className="chat__header">
@@ -34,23 +50,31 @@ const Chat = () => {
       </div>
 
       <div className="chat__body">
-        <p className="chat__message">
-          <span className="chat__name">Anas</span>
-          This is a message
-          <span className="chat__timestamp">{new Date().toUTCString()}</span>
-        </p>
-
-        <p className="chat__message chat__receiver">
-          <span className="chat__name">Anas</span>
-          This is a message
-          <span className="chat__timestamp">{new Date().toUTCString()}</span>
-        </p>
+        {messages.map((message) => {
+          return (
+            <p
+              className={`chat__message ${
+                message.receiver && "chat__receiver"
+              }`}
+              key={message._id}
+            >
+              <span className="chat__name">{message.name}</span>
+              {message.message}
+              <span className="chat__timestamp">{message.timestamp}</span>
+            </p>
+          );
+        })}
       </div>
 
       <div className="chat__footer">
         <EmojiEmotionsOutlinedIcon />
-        <form action="">
-          <input type="text" placeholder="Type a message" />
+        <form>
+          <input
+            type="text"
+            placeholder="Type a message"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
           <button type="submit" onClick={sendMessage}>
             Send a message
           </button>
